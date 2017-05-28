d59b61e630326b4db068720c47bec5e51ab6bfcc
Varik <varikmatevosyan@gmail.com>
2017-05-28 15:40:36 +0400

    save song duration in json

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import pafy
 import os
-
+import json
 
 SERVER_URL = 'http://52.232.85.160/'
 EXT = ".m4a"
@@ -8,9 +8,10 @@ EXT = ".m4a"
 def download_audio(video_id):
     if not video_id:
         return {"status": False, "error": "Video id not specified"}
+    song = get_song(video_id)
 
-    if os.path.exists('./dist/' + video_id + EXT):
-        return {"status": True, "url": SERVER_URL + video_id}
+    if song:
+        return song
 
     try:
         video = pafy.new(str(video_id))
@@ -22,5 +23,33 @@ def download_audio(video_id):
     if len(streams) > 0:
         filename = video.getbestaudio(preftype=EXT[1:]).download(filepath="./dist", quiet=True)
         os.rename(filename, './dist/' + video_id + EXT)
-        return {"status": True, "url": SERVER_URL + video_id}
+        song_url = SERVER_URL + video_id
+        song = add_song(video_id, get_sec(video.duration), song_url)
+        return song
     return {"status": False, "error": "No audio streams found"}
+
+
+def add_song(id, duration, url):
+    with open('songs.json', 'a') as file:
+        try:
+          songs = json.loads(file)
+        except:
+          songs = {}
+        songs[id] = {"duration": duration, "url": url}
+        print(songs)
+        json.dump(songs, file)
+        return songs[id]
+
+
+def get_sec(time_str):
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+def get_song(id):
+    with open('songs.json', 'a') as file:
+        try:
+            songs = json.load(file)
+            print(songs)
+            return songs[id]
+        except:
+            return None
